7323c095265b6f7bb600461c47e57d36296221a6
Bernadette <bernadettemetran35@gmail.com>
2023-09-25 22:56:07 -0700

    first commit

---
+++ multiple conditional(ternary)operators.js
@@ -0,0 +1,12 @@
+//Use Multiple Conditional (Ternary) Operators
+
+/*In the checkSign function, use multiple conditional operators. The function shld return
+positive, negative or zero. */
+
+function checkSign(num) {
+    return (num > 0) ? "positive"
+    : (num < 0) ? "negative"
+    : "zero";
+}
+
+checkSign(10);
